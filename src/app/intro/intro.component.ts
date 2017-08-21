5463f98d4c31ded7a584e5fdc2d15e859c400be8
Robinson Manonmani <robinson.manonmani@gmail.com>
2017-08-20 22:44:00 -0400

    intro animation

--- src/app/intro/intro.component.ts
+++ src/app/intro/intro.component.ts
@@ -9,5 +9,35 @@ import { Component, OnInit  } from '@angular/core';
 
 export class Intro implements OnInit{
     ngOnInit(){
+		(function() {
+			var app;
+		  
+			$(document).ready(function() {
+			  return app.init();
+			});
+		  
+			app = {
+			  text: "Hi I'm a UI Developer. I like to code, automate stuff and do fun things..",
+			  index: 0,
+			  chars: 0,
+			  speed: 100,
+			  container: ".text .content",
+			  init: function() {
+				this.chars = this.text.length;
+				return this.write();
+			  },
+			  write: function() {
+				$(this.container).append(this.text[this.index]);
+				if (this.index < this.chars) {
+				  this.index++;
+				  return window.setTimeout(function() {
+					return app.write();
+				  }, this.speed);
+				}
+			  }
+			};
+		  
+		  }).call(this);
+		  
 	}   
 }
